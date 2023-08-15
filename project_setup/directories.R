b03990ecf74a1db9a4a8552bae4dfb47e78d66af
Jesse Goodrich <jagoodri@usc.edu>
2023-08-14 22:24:35 -0700

    update directories

--- project_setup/directories.R
+++ project_setup/directories.R
@@ -1,19 +1,18 @@
 # directories
 
-# home directory for project
-dir_home <- here::here() 
+# project setup directory
+dir_proj <- fs::path(here::here() , "project_setup") 
 
 # Mercury Data directory
-dir_data_hg <- fs::path(dir_home, "0_data")
+dir_data_hg <- fs::path(here::here() , "data")
 
 # results folder
-dir_results <- fs::path(dir_home, "2_results")
+dir_results <- fs::path(here::here() , "2_results")
 
 # figures 
-dir_figs <- fs::path(dir_home, "figures")
+dir_figs <- fs::path(here::here() , "figures")
 
 # functions
 dir_fxn <- fs::path(here::here(), 
-                    "0_project_setup",
+                    "project_setup",
                     "functions")
-rm(dir_home)
